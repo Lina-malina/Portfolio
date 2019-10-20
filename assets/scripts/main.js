120a899dea9b2d47c0ac7cd23813f4a7c3a7f9ae
Angelina Pobyvanets <gela4w@gmail.com>
2019-10-20 20:58:38 +0300

    init: initial implementation of portfolio

---
+++ assets/scripts/main.js
@@ -0,0 +1,28 @@
+let accordion = document.querySelector('.education_accordion');
+
+accordion.addEventListener('click', function () {
+    document.querySelector('.education_panel').classList.toggle('visible');
+});
+
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+function changeDescription(button) {
+    button.innerText = button.innerText == 'Show description' ? 'Hide description' : 'Show description';
+    let descriptionBlock = button.parentNode.querySelector('.mobile_slide--description');
+    descriptionBlock.classList.toggle('visible');
+}
