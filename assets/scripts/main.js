000f8f16c52efacf2c80d13b8571f74e31f8da25
Angelina Pobyvanets <gela4w@gmail.com>
2019-12-01 17:54:13 +0300

    refactor: refactor with bootstrap 4

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,29 +1,16 @@
-let accordion = document.querySelector('.education_accordion');
-
-accordion.addEventListener('click', function () {
-    document.querySelector('.education_panel').classList.toggle('visible');
+$('.carousel').carousel({touch: true});
+const showBtnRepair = document.getElementById('show_btn_repair');
+showBtnRepair.addEventListener('click', () => {
+    const mobileDescription = document.getElementById('mobile_description');
+    if(mobileDescription.classList.contains('hidden')) {
+        mobileDescription.classList.toggle('visible');
+    }
+});
+const showBtnTheyalow = document.getElementById('show_btn_theyalow');
+showBtnTheyalow.addEventListener('click', () => {
+    const mobileDescriptionTheyalow = document.getElementById('mobile_description_theyalow');
+    if(mobileDescriptionTheyalow.classList.contains('hidden')) {
+        mobileDescriptionTheyalow.classList.toggle('visible');
+    }
 });
 
-var mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
-
-function changeDescription(button) {
-    button.innerText = button.innerText == 'Show description' ? 'Hide description' : 'Show description';
-    let descriptionBlock = button.parentNode.querySelector('.mobile_slide--description');
-    descriptionBlock.classList.toggle('visible');
-}
