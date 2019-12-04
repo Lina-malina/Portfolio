bcd49c4f538d6963394b86cccd59ced3c6730b7b
Angelina Pobyvanets <gela4w@gmail.com>
2019-12-04 11:18:00 +0300

    fix: fix description block in projects swiper, add hide button

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,6 +4,7 @@ showBtnRepair.addEventListener('click', () => {
     const mobileDescription = document.getElementById('mobile_description');
     if(mobileDescription.classList.contains('hidden')) {
         mobileDescription.classList.toggle('visible');
+        showBtnRepair.innerText = showBtnRepair.innerText == 'Show description' ? 'Hide description' : 'Show description';
     }
 });
 const showBtnTheyalow = document.getElementById('show_btn_theyalow');
@@ -11,6 +12,7 @@ showBtnTheyalow.addEventListener('click', () => {
     const mobileDescriptionTheyalow = document.getElementById('mobile_description_theyalow');
     if(mobileDescriptionTheyalow.classList.contains('hidden')) {
         mobileDescriptionTheyalow.classList.toggle('visible');
+        showBtnTheyalow.innerText = showBtnTheyalow.innerText == 'Show description' ? 'Hide description' : 'Show description';
     }
 });
 
